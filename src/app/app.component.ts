a1bf35006252f8af0f76bb3b8c11eaea4c9b8e17
RicDev116 <ricardo_29leyva@outlook.com>
2024-01-28 21:44:55 -0600

    inicializacion de primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+import {ButtonModule} from 'primeng/button';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pipesApp';
+  constructor(private primengConfig:PrimeNGConfig){}
 }
