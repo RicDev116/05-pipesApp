8e3aa351ccbe8e192755cb676f06bf79f58a17eb
RicDev116 <ricardo_29leyva@outlook.com>
2024-02-10 17:10:34 -0600

    Navegar usando el menubar

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -17,15 +17,18 @@ export class MenuComponent{
         items: [
           {
             label: 'Textos y Fechas',
-            icon: 'pi pi-align-left'
+            icon: 'pi pi-align-left',
+            url:'/'
           },
           {
             label: 'Números',
-            icon: 'pi pi-dollar'
+            icon: 'pi pi-dollar',
+            url:'/numbers',
           },
           {
             label: 'No comunes',
-            icon: 'pi pi-globe'
+            icon: 'pi pi-globe',
+            url:'/uncommon',
           }
         ]
       },
